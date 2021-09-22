5c58008de5be3f46814c5b5d574b7687b271d3e6
Mandel <frede.p@live.dk>
2021-09-22 16:20:57 +0200

    First commit, empty project and .gitignore

---
+++ Assets/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+
+    public Snake segments;
+    public Text scoreText;
+    public Text highscoreText;
+    public List<int> highScore = new List<int>();
+
+    void Update()
+    {
+        scoreText.text = "SCORE: " + (segments._segments.Count - 3).ToString();
+    }
+
+    public void HighScoreUpdate(int newScore)
+    {
+        highScore.Add(newScore);
+        highScore.Sort();
+        highScore.Reverse();
+        highscoreText.text = "HIGHSCORE\n" + string.Join("\n", highScore).ToString();
+    }
+}
